86c196257a840a7d3ed942f1d8c81a8effe905fc
dbseend <141024382+dbseend@users.noreply.github.com>
2024-02-29 02:37:20 +0900

    Khj (#8)
    * make gardenlevel flow .v1
    
    * make gardeninfo
    
    ---------
    
    Co-authored-by: Hajin <22100228@handong.ac.kr>

--- src/main/java/com/seesun/andand/garden/service/GardenService.java
+++ src/main/java/com/seesun/andand/garden/service/GardenService.java
@@ -1,9 +1,32 @@
 package com.seesun.andand.garden.service;
 
+import com.seesun.andand.appUserMate.domain.AppUserMate;
+import com.seesun.andand.garden.domain.GardenRepository;
+import com.seesun.andand.garden.dto.response.GardenInfo;
+import com.seesun.andand.mate.domain.Mate;
+import com.seesun.andand.mate.domain.MateRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class GardenService {
+    private final MateRepository mateRepository;
+
+    public GardenInfo getGardenInfo(Long mateId) {
+        Mate mate = mateRepository.findById(mateId).orElseThrow(() -> new IllegalArgumentException("해당 메이트가 없습니다."));
+        Integer gardenLevel = mate.getGardenLevel();
+
+        Integer gardenNum = mate.getGardenNum();
+
+        List<AppUserMate> appUserMates = mate.getAppUserMateList();
+
+        List<String> appUserNameList = appUserMates.stream().map(appUserMate -> appUserMate.getAppUser().getName()).toList();
+
+        return new GardenInfo(gardenLevel, gardenNum, appUserNameList);
+    }
 }
