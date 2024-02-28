ab3ded2d035cdba818782b1a1b356f65dc851021
dbseend <141024382+dbseend@users.noreply.github.com>
2024-02-28 21:04:25 +0900

    [FEAT]: 상대방과 연결 메소드 구현 (#7)
    * [FEAT]: 회원가입 사진 업로드 기능 추가
    
    * [FEAT]: 아이디 중복 검사 메소드 추가
    
    * [FEAT]: 사용자 조회 구현
    
    * [FEAT]: 회원정보 수정: 그룹 정보 볼 수 있도록 수정
    
    * [FEAT]: 상대방과 연결 메소드 구현
    
    * [FEAT]: 상대방과 연결 메소드 구현

--- src/main/java/com/seesun/andand/mate/service/MateService.java
+++ src/main/java/com/seesun/andand/mate/service/MateService.java
@@ -9,41 +9,17 @@ import com.seesun.andand.mate.domain.Mate;
 import com.seesun.andand.mate.domain.MateRepository;
 import com.seesun.andand.mate.dto.request.ConnectMateRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MateService {
 
     private final MateRepository mateRepository;
-    private final AppUserRepository appUserRepository;
-    private final AppUserMateRepository appUserMateRepository;
     private final AppUserSubService appUserSubService;
     private final MateSubService mateSubService;
-
-    // 메이트 등록 메소드
-    public void connectMate(ConnectMateRequest connectMateRequest) {
-
-        AppUser appUser = appUserSubService.findById(connectMateRequest.getAppUserId());
-
-        Mate mate = mateSubService.findMateByCode(connectMateRequest.getUserCode());
-
-        mateSubService.connectMate(appUser, mate);
-    }
-
-    // 메이트 삭제 메소드
-    public void disconnectMate(Long appUserId, Long mateId) {
-
-        // 사용자 정보 조회
-        AppUser appUser = appUserRepository.findById(appUserId).
-                orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다."));
-        // 메이트 정보 조회
-        Mate mate = mateRepository.findById(mateId).
-                orElseThrow(() -> new IllegalArgumentException("해당 메이트가 없습니다."));
-
-        // 사용자와 메이트 연결 삭제
-        AppUserMate appUserMate = appUserMateRepository.findByAppUserAndMate(appUser, mate).
-                orElseThrow(() -> new IllegalArgumentException("해당 사용자와 메이트 연결이 없습니다."));
-        appUserMateRepository.delete(appUserMate);
-    }
 }
